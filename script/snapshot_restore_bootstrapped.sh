a795a81db0d1e354a4dc6eb17828c6253de3ab3a
Toshifumi Kurosawa <bizp96@gmail.com>
2018-11-22 14:41:48 +0900

    文言修正

--- script/snapshot_restore_bootstrapped.sh
+++ script/snapshot_restore_bootstrapped.sh
@@ -3,9 +3,9 @@
 # デバッグを開始する
 # set -x
 
-echo -e $'\e[1;31m ================================= \e[m'
-echo -e $'\e[1;31m vagrant up \e[m'
-echo -e $'\e[1;31m ================================= \e[m'
+echo -e $'\e[1;31m ====================================== \e[m'
+echo -e $'\e[1;31m vagrant snapshot restore bootstrapped  \e[m'
+echo -e $'\e[1;31m ====================================== \e[m'
 vagrant snapshot restore bootstrapped
 
 # デバッグを終了する
