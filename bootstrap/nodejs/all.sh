331dbf9b44be8112236856d34cae3832c364b4cb
Toshifumi Kurosawa <bizp96@gmail.com>
2018-11-22 16:40:46 +0900

    パス修正

--- bootstrap/nodejs/all.sh
+++ bootstrap/nodejs/all.sh
@@ -6,17 +6,17 @@
 echo -e $'\e[1;31m ================================= \e[m'
 echo -e $'\e[1;31m update_nodejs           　 　      \e[m'
 echo -e $'\e[1;31m ================================= \e[m'
-./update_nodejs.sh
+./bootstrap/nodejs/update_nodejs.sh
 
 echo -e $'\e[1;31m ================================= \e[m'
 echo -e $'\e[1;31m db_mongo.sh             　 　      \e[m'
 echo -e $'\e[1;31m ================================= \e[m'
-./db_mongo.sh
+./bootstrap/nodejs/db_mongo.sh
 
 echo -e $'\e[1;31m ================================= \e[m'
 echo -e $'\e[1;31m express.sh              　 　      \e[m'
 echo -e $'\e[1;31m ================================= \e[m'
-./express.sh
+./bootstrap/nodejs/express.sh
 
 # デバッグを終了する
 set +x
