c46a0b317cc7d1cfb23cf338bf8bb16ce5a2726d
Toshifumi Kurosawa <bizp96@gmail.com>
2018-11-22 16:22:43 +0900

    node.js mongodb追加

---
+++ bootstrap/nodejs/update_nodejs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# デバッグを開始する
+# set -x
+
+echo -e $'\e[1;31m ================================= \e[m'
+echo -e $'\e[1;31m nodejs                  　 　      \e[m'
+echo -e $'\e[1;31m ================================= \e[m'
+node -v
+
+curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+sudo yum -y install nodejs
+
+ls -la /etc/yum.repos.d/ | grep node
+sudo  rm /etc/yum.repos.d/nodesource-el.repo
+sudo yum -y remove nodejs
+sudo yum clean all
+
+sudo yum -y install nodejs
+node -v
+
+# デバッグを終了する
+set +x
